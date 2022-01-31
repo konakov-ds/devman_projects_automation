afb56e4d7790e48953da6ed0a472b2df8e1088ea
konakov-ds <konakov-ds@yandex.ru>
2022-01-31 15:05:34 +0300

    Update handlers.py

--- projects_automation/automation/handlers.py
+++ projects_automation/automation/handlers.py
@@ -225,16 +225,15 @@ def update_student_time(update, context):
             reply_markup=ReplyKeyboardRemove()
         )
         return ConversationHandler.END
-    elif message.text == 'Не смогу участвовать в проекте':
-        student = Student.objects.get(tg_id=user_id)
-        student.group = ''
-        student.save()
-        context.bot.send_message(
-            chat_id=user_id,
-            text=f'Очень жаль :( Тогда ждем тебя на следующем!',
-            reply_markup=ReplyKeyboardRemove()
-        )
-        return ConversationHandler.END
+    student = Student.objects.get(tg_id=user_id)
+    student.group = ''
+    student.save()
+    context.bot.send_message(
+        chat_id=user_id,
+        text=f'Очень жаль :( Тогда ждем тебя на следующем!',
+        reply_markup=ReplyKeyboardRemove()
+    )
+    return ConversationHandler.END
 
 
 def save_pm_input(update, context):
